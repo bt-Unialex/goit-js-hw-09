a8ca82dd9169ae1995fa0fa632e96fa3e837194f
Oleksiy <Unialex@ukr.net>
2025-02-05 16:49:24 +0200

    form done

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+const formData = {
+  email: '',
+  message: '',
+};
+const feedbackFormState = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+);
+if (feedbackFormState) {
+  Object.assign(formData, feedbackFormState);
+}
+
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.elements.email.value = formData.email;
+feedbackForm.elements.message.value = formData.message;
+
+feedbackForm.addEventListener('input', event => {
+  if (event.target.nodeName === 'INPUT') {
+    formData.email = feedbackForm.elements.email.value;
+  } else if (event.target.nodeName === 'TEXTAREA') {
+    formData.message = feedbackForm.elements.message.value;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email && formData.message) {
+    console.table(formData);
+    feedbackForm.reset();
+    localStorage.removeItem('feedback-form-state');
+  } else {
+    alert('Fill please all fields');
+  }
+});
